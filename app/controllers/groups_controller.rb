bac38ead5736360e9912a17cf35c170826427d6c
yimiao <345230916@qq.com>
2017-01-07 14:26:03 +0800

    check ower permission when access edit/update/destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create]
-  
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+
   def index
     @groups = Group.all
   end
@@ -11,6 +11,9 @@ end
 
 def edit
   @group = Group.find(params[:id])
+
+  if current_user != @group.user
+    redirect_to root_path, alert: "You have no permission."
 end
 
   def new
@@ -30,6 +33,9 @@ end
 
 def update
   @group = Group.find(params[:id])
+  if current_user != @group.user
+    redirect_to root_path, alert: "You have no permission."
+end
 
   if @group.update(group_params)
     redirect_to groups_path, notice: "Update Success"
@@ -40,6 +46,11 @@ end
 
 def destroy
   @group = Group.find(params[:id])
+
+  if current_user != @group.user
+    redirect_to root_path, alert: "You have no permission."
+end
+
   @group.destroy
   flash[:alert] = "Group deleted"
   redirect_to groups_path
